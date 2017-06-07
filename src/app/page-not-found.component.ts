558432220be93e0e7ba163fd7b4b5bd5a35cd947
Jun <Jun@Junui-MacBook-Pro.local>
2017-06-07 13:40:32 +0900

    changed page-not-found

--- src/app/page-not-found.component.ts
+++ src/app/page-not-found.component.ts
@@ -5,12 +5,8 @@ import { MockDataService } from './shared/mockdata.service';
 
 @Component({
   selector: 'app-page-not-found',
-  template: `
-    <p>
-      This Page is not found 
-    </p>
-  `,
-  styles: []
+  templateUrl: './page-not-found.component.html',
+  styleUrls: ['./page-not-found.component.scss']
 })
 export class PageNotFoundComponent implements OnInit {
 
@@ -30,23 +26,27 @@ export class PageNotFoundComponent implements OnInit {
           this.snapshot = activatedRoute.snapshot.url;
         })
 
-  }
-
-  ngOnInit() {
-
     this.redirect = this.mockDataService.redirect;
 
     this.found = this.redirect.find( item => {
       return item.old == this.snapshot.join('/') 
     }) 
 
-    if(this.found == undefined) {
+    if(this.snapshot[0] == 'home' || this.snapshot[0] == 'main') {
+      //nothing
+      //show this not-found-page
+    } else if (this.found == undefined) {
       //relocate to old homepage, with original address
       window.location.href="http://vml2.kaist.ac.kr/" + this.snapshot.join('/');
     } else {
       //relocate to new homepage, with different address
       window.location.href= this.found.new;
     }
+
+
+  }
+
+  ngOnInit() {
 
   }
 
